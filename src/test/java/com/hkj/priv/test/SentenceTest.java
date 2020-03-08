7a4f16c79d4998aef08ba4f731f91b5378c2b35c
jia1991 <568186932@qq.com>
2020-03-08 16:23:42 +0800

    add test

--- src/test/java/com/hkj/priv/test/SentenceTest.java
+++ src/test/java/com/hkj/priv/test/SentenceTest.java
@@ -1,4 +1,23 @@
 package com.hkj.priv.test;
 
+import com.hkj.priv.Dictionary;
+import com.hkj.priv.Sentence;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class SentenceTest {
+    @Test
+    public void wordbreak() {
+        Sentence.wordBreak("ilikesamsungmobile");
+        Sentence.wordBreak("ilikeicecreamandmango");
+    }
+
+    @Before
+    public void BeforeBreak () {
+        List newDict = Arrays.asList("i", "like", "sam", "sung", "samsung", "mobile", "ice", "cream", "mango");
+        Dictionary.updateDict(newDict);
+    }
 }
